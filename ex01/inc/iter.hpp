94f1d3565ee858f52db53ff02d94d1fed427433f
Niklas Weber <nweber@student.42Heilbronn.de>
2025-11-17 13:13:37 +0100

    add: iter implementation

---
+++ ex01/inc/iter.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <string>
+#include <iostream>
+
+template <typename T>
+void iter(T* array, size_t length, void (*func)(T&))
+{
+  for (size_t i = 0; i < length; i++)
+  {
+    func(array[i]);
+  }
+}
+
+template <typename T>
+void iter(const T* array, size_t length, void (*func)(const T&))
+{
+  for (size_t i = 0; i < length; i++)
+  {
+    func(array[i]);
+  }
+}
