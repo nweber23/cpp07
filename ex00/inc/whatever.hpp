970ec814a1832f39977195ffdbc7127c8fac2662
Niklas Weber <nweber@student.42Heilbronn.de>
2025-11-17 12:50:30 +0100

    finished ex00

--- ex00/inc/whatever.hpp
+++ ex00/inc/whatever.hpp
@@ -1,1 +1,24 @@
 #pragma once
+
+#include <string>
+#include <iostream>
+
+template <typename T>
+void swap(T& a, T& b)
+{
+	t tmp = a;
+	a = b;
+	b = tmp;
+}
+
+template <typename T>
+T min(const T& a, const T& b)
+{
+	return (a < b ? a : b);
+}
+
+template <typename T>
+T max(const T& a, const T& b)
+{
+	return (a > b ? a : b);
+}
