fb17145db6951227e87912d3396335464ce568f9
Niklas Weber <nweber@student.42Heilbronn.de>
2025-12-15 11:16:12 +0100

    updated: length to a const as required by subject

--- ex01/inc/iter.hpp
+++ ex01/inc/iter.hpp
@@ -4,7 +4,7 @@
 #include <iostream>
 
 template <typename T>
-void iter(T* array, size_t length, void (*func)(T&))
+void iter(T* array,  const size_t length, void (*func)(T&))
 {
   for (size_t i = 0; i < length; i++)
   {
@@ -13,7 +13,7 @@ void iter(T* array, size_t length, void (*func)(T&))
 }
 
 template <typename T>
-void iter(const T* array, size_t length, void (*func)(const T&))
+void iter(const T* array, const size_t length, void (*func)(const T&))
 {
   for (size_t i = 0; i < length; i++)
   {
